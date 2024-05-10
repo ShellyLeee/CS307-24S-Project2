b1c260f6822903fe8af65c85eec93b1d1ab8dff0
JackTianyuLi <12212824@mail.sustech.edu.cn>
2024-05-10 09:22:19 +0800

    try

--- CS307-24Spring-Project2/src/test/java/cs307/cs30724springproject2/Cs30724SpringProject2ApplicationTests.java
+++ CS307-24Spring-Project2/src/test/java/cs307/cs30724springproject2/Cs30724SpringProject2ApplicationTests.java
@@ -1,6 +1,6 @@
 package cs307.cs30724springproject2;
 
-import org.junit.jupiter.api.Test;
+import org.testng.annotations.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
