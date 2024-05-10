9335224f1f15a764d68f4c967d8694c7007dc665
ShellyLi <112748222+ShellyLeee@users.noreply.github.com>
2024-05-10 08:44:41 +0800

    Add files via upload

---
+++ CS307-24Spring-Project2/src/test/java/cs307/cs30724springproject2/Cs30724SpringProject2ApplicationTests.java
@@ -0,0 +1,13 @@
+package cs307.cs30724springproject2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cs30724SpringProject2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
