3b7eb138e60ca60c91f304160e9bbaa1e92be26e
JackTianyuLi <12212824@mail.sustech.edu.cn>
2024-05-13 09:41:34 +0800

    5.12 modification

---
+++ CS307-24Spring-Project2/src/main/java/cs307/cs30724springproject2/DeskTopGUI.java
@@ -0,0 +1,17 @@
+package cs307.cs30724springproject2;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.swing.*;
+
+
+@Component
+public class DeskTopGUI extends JFrame {
+    @Autowired
+    private ;
+
+    public void init(){
+
+    }
+}
