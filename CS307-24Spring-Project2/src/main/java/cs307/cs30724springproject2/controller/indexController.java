07fdb6f9b0e4a62c2a0cc6145c580555198e2d68
ShellyLi <12212959@mail.sustech.edu.cn>
2024-05-28 12:10:09 +0800

    Updated: html templates and multi-parameter select sql
    1. 计划将html网页主页面设置为index.html, 通过选择是管理员端、用户端来进入不同的页面，进行操作（按钮不一样，数据库连接不一样），但还没有完全改完增加完，待完善
    2. 动态多参数sql查询大概知道怎么写了，但是还没写完
    3. 准备修改一下点选按钮后出来的弹窗模式；写在script.html里，还没有正式改上去

--- CS307-24Spring-Project2/src/main/java/cs307/cs30724springproject2/controller/indexController.java
+++ CS307-24Spring-Project2/src/main/java/cs307/cs30724springproject2/controller/indexController.java
@@ -2,9 +2,10 @@ package cs307.cs30724springproject2.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class IndexController {
+public class indexController {
 
     @GetMapping("/")
     public String index() {
