db8810589870de4f2a011e602c3d254780470577
ShellyLi <12212959@mail.sustech.edu.cn>
2024-06-03 22:54:33 +0800

    Updated

--- CS307-24Spring-Project2/src/main/java/cs307/cs30724springproject2/controller/lineDetailController.java
+++ CS307-24Spring-Project2/src/main/java/cs307/cs30724springproject2/controller/lineDetailController.java
@@ -5,7 +5,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import cs307.cs30724springproject2.service.lineDetailService;
+import cs307.cs30724springproject2.entity.station;
 
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -15,15 +17,26 @@ public class lineDetailController {
 
     @PostMapping("/updateInsertStationNoBehind")
     public void updateInsertStationNoBehind(@RequestBody Map<String, Object> parameters) {
-        int n = (int) parameters.get("n");
+        String nStr = (String) parameters.get("n");
+        int n = Integer.parseInt(nStr);
         String lineName = (String) parameters.get("lineName");
         String stationName = (String) parameters.get("stationName");
+        System.out.println(n + " " + lineName + " " + stationName);
         lineDetailService.updateInsertStationNoBehind(n, lineName, stationName);
     }
 
+    @PostMapping("/insertMultipleStationsBehind")
+    public void insertMultipleStations(@RequestBody Map<String, Object> request) {
+        String lineName = (String) request.get("lineName");
+        List<Map<String, Object>> stations = (List<Map<String, Object>>) request.get("stations");
+        lineDetailService.insertMultipleStationsBehind(lineName, stations);
+    }
+
+
     @PostMapping("/updateInsertStationNoFront")
     public void updateInsertStationNoFront(@RequestBody Map<String, Object> parameters) {
-        int n = (int) parameters.get("n");
+        String nStr = (String) parameters.get("n");
+        int n = Integer.parseInt(nStr);
         String lineName = (String) parameters.get("lineName");
         String stationName = (String) parameters.get("stationName");
         lineDetailService.updateInsertStationNoFront(n, lineName, stationName);
--- CS307-24Spring-Project2/src/main/java/cs307/cs30724springproject2/service/lineDetailService.java
+++ CS307-24Spring-Project2/src/main/java/cs307/cs30724springproject2/service/lineDetailService.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import cs307.cs30724springproject2.mapper.lineDetailMapper;
 
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Service
@@ -14,7 +16,12 @@ public class lineDetailService {
     public void updateInsertStationNoBehind(int n, String lineName, String stationName) {
         lineDetailMapper.updateInsertStationNoBehind(Map.of("n", n, "lineName", lineName, "stationName", stationName));
     }
-
+    public void insertMultipleStationsBehind(String lineName, List<Map<String, Object>> stations) {
+        for (Map<String, Object> station : stations) {
+            System.out.println("Station info: " + station);
+        }
+        lineDetailMapper.insertMultipleStationsBehind(lineName, stations);
+    }
     public void updateInsertStationNoFront(int n, String lineName, String stationName) {
         lineDetailMapper.updateInsertStationNoFront(Map.of("n", n, "lineName", lineName, "stationName", stationName));
     }
