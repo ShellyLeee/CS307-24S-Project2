8b06e42aac40259d14cbf647626aa6d0f2309a8e
JackTianyuLi <12212824@mail.sustech.edu.cn>
2024-06-04 23:30:18 +0800

    Update: 6(未实现）

--- CS307-24Spring-Project2/src/main/java/cs307/cs30724springproject2/controller/businessPriceController.java
+++ CS307-24Spring-Project2/src/main/java/cs307/cs30724springproject2/controller/businessPriceController.java
@@ -8,6 +8,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/businessPrice")
@@ -15,8 +16,10 @@ public class businessPriceController {
     @Autowired
     public businessPriceService businessPriceService;
 
+    @ResponseBody
     @GetMapping(value = "/selectByStations", produces = "application/json;charset=utf-8")
-    public businessPrice selectByStations(@RequestParam String start, String end, Model model){
+    public businessPrice selectByStations(@RequestParam String start,
+                                          @RequestParam String end, Model model){
         // 这里要改
         businessPrice businessPrice = businessPriceService.getprice(start, end);
         model.addAttribute("businessPrice", businessPrice);
--- CS307-24Spring-Project2/src/main/java/cs307/cs30724springproject2/controller/priceController.java
+++ CS307-24Spring-Project2/src/main/java/cs307/cs30724springproject2/controller/priceController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/price")
@@ -16,6 +17,7 @@ public class priceController {
     @Autowired
     public priceService priceService;
 
+    @ResponseBody
     @GetMapping(value = "/selectByStations", produces = "application/json;charset=utf-8")
     public price selectByStations(@RequestParam String start,
                                   @RequestParam String end, Model model){
