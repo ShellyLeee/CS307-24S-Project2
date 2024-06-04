e892e91dd03b172994337794063cc60c3b1b92cc
ShellyLi <12212959@mail.sustech.edu.cn>
2024-06-04 21:05:47 +0800

    Update: 多参数查询Passenger补全
    多参数查询完全写完

--- CS307-24Spring-Project2/src/main/java/cs307/cs30724springproject2/controller/passengerRideController.java
+++ CS307-24Spring-Project2/src/main/java/cs307/cs30724springproject2/controller/passengerRideController.java
@@ -1,16 +1,19 @@
 package cs307.cs30724springproject2.controller;
 
+import cs307.cs30724springproject2.entity.cardRide;
 import cs307.cs30724springproject2.entity.passengerRide;
 import cs307.cs30724springproject2.service.passengerRideService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+import java.time.LocalDateTime;
+import java.util.List;
+
+@RestController
 @RequestMapping("/passengerRide")
 public class passengerRideController {
     @Autowired
@@ -24,5 +27,19 @@ public class passengerRideController {
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to insert ride.");
         }
+    }
+
+    @GetMapping( "/multiSelect")
+    public List<passengerRide> getPassengerRides(
+            @RequestParam(required = false) String passengerId,
+            @RequestParam(required = false) String startStation,
+            @RequestParam(required = false) String endStation,
+            @RequestParam(required = false) Integer price,
+            @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime startTime,
+            @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime endTime
+    ) {
+        System.out.println(passengerId);
+        List<passengerRide> target = passengerRideService.getPassengerRidesByParameters(passengerId, startStation, endStation, price, startTime, endTime);
+        return target;
     }
 }
--- CS307-24Spring-Project2/src/main/java/cs307/cs30724springproject2/service/passengerRideService.java
+++ CS307-24Spring-Project2/src/main/java/cs307/cs30724springproject2/service/passengerRideService.java
@@ -1,5 +1,6 @@
 package cs307.cs30724springproject2.service;
 
+import cs307.cs30724springproject2.entity.cardRide;
 import cs307.cs30724springproject2.entity.passengerRide;
 import cs307.cs30724springproject2.mapper.priceMapper;
 import cs307.cs30724springproject2.mapper.passengerRideMapper;
@@ -7,6 +8,9 @@ import cs307.cs30724springproject2.mapper.businessPriceMapper;
 import cs307.cs30724springproject2.mapper.passengerOnboardMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
 public class passengerRideService {
@@ -25,5 +29,9 @@ public class passengerRideService {
 
     public void insert(passengerRide passengerRide) {
         passengerRideMapper.insert(passengerRide);
+    }
+
+    public List<passengerRide> getPassengerRidesByParameters(String passengerId, String startStation, String endStation, Integer price, LocalDateTime startTime, LocalDateTime endTime) {
+        return passengerRideMapper.selectPassengerRideByParameters(passengerId, startStation, endStation, price, startTime, endTime);
     }
 }
