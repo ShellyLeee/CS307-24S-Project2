05e6e34e720d24a4b787ece79162b7d4002acad6
JackTianyuLi <12212824@mail.sustech.edu.cn>
2024-06-04 19:15:18 +0800

    update 7搞定

--- CS307-24Spring-Project2/src/main/java/cs307/cs30724springproject2/controller/cardOnboardController.java
+++ CS307-24Spring-Project2/src/main/java/cs307/cs30724springproject2/controller/cardOnboardController.java
@@ -1,18 +1,15 @@
 package cs307.cs30724springproject2.controller;
 
 import cs307.cs30724springproject2.entity.cardOnboard;
-import cs307.cs30724springproject2.entity.passengerOnboard;
 import cs307.cs30724springproject2.service.cardOnboardService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.ui.Model;
 
 import java.util.List;
 
-@Controller
+@RestController
 @RequestMapping("/cardOnboard")
 public class cardOnboardController {
     @Autowired
--- CS307-24Spring-Project2/src/main/java/cs307/cs30724springproject2/controller/passengerOnboardController.java
+++ CS307-24Spring-Project2/src/main/java/cs307/cs30724springproject2/controller/passengerOnboardController.java
@@ -5,9 +5,7 @@ import cs307.cs30724springproject2.service.passengerOnboardService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.ui.Model;
 
 import java.util.List;
 
